cb26b3f66050493d5456daa42f1e94b02b86085c
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-05-30 14:50:16 +0800

    Added onLoad(), onDisable(), saveFiles()

--- src/HypeX/FloatingText/Main.php
+++ src/HypeX/FloatingText/Main.php
@@ -2,37 +2,56 @@
 
 namespace HypeX\FloatingText;
 
+use pocketmine\Server;
+use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\level\particle\FloatingTextParticle;
-use pocketmine\math\Vector3;
+use pocketmine\level\particle\Particle;
 use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\level\Position\getLevel;
+use pocketmine\plugin\PluginManager;
+use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
-use pocketmine\Server;
-use pocketmine\Player;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
 
 class Main extends PluginBase implements Listener{
 
+   public function onLoad(){
+        $this->getLogger()->info(TextFormat::GREEN. "FloatingText has been enabled.");  
+   }
+   public function saveFiles(){
+	if(!file_exists($this->getDataFolder())){
+		mkdir($this->getDataFolder());
+	}
+   }
    public function onEnable(){
         $this->saveDefaultConfig();
-		  $this->reloadConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->onLevel($this->getServer()->getDefaultLevel());
-   } 
-   public function onLevel(Level $level){
+        @mkdir($this->getDataFolder());
+        $this->cfg = $this->getConfig();
+   }
+   public function onDisable(){
+	unset($this->players);
+	$this->saveDefaultConfig();
+	$this->getLogger()->info(TextFormat::RED. "FloatingText has been disabled.");
+   }
+   public function onPlayerJoin(PlayerJoinEvent $event){
         $cfg = $this->getConfig();
-		  $pos11 = $cfg->get("pos1");
-		  $pos22 = $cfg->get("pos2");
-		  $pos33 = $cfg->get("pos3");
-		  $pos44 = $cfg->get("pos4");
-		  $pos55 = $cfg->get("pos5");
+        $pos11 = $cfg->get("pos1");
+	$pos22 = $cfg->get("pos2");
+	$pos33 = $cfg->get("pos3");
+	$pos44 = $cfg->get("pos4");
+	$pos55 = $cfg->get("pos5");
 		  
-		  $text1 = $cfg->get("text1");
-		  $text2 = $cfg->get("text2");
-		  $text3 = $cfg->get("text3");
-		  $text4 = $cfg->get("text4");
-		  $text5 = $cfg->get("text5");
+	$text1 = $cfg->get("text1");
+	$text2 = $cfg->get("text2");
+	$text3 = $cfg->get("text3");
+	$text4 = $cfg->get("text4");
+	$text5 = $cfg->get("text5");
 		  
         $pos1 = new Vector3($pos11);
         $pos2 = new Vector3($pos22);
