a7a46248a41d3d0b48054abba826cf4d7bec8fe2
Anton Magsino <MinecrafterPH@users.noreply.github.com>
2015-05-30 14:53:02 +0800

    Update Main.php

--- src/HypeX/FloatingText/Main.php
+++ src/HypeX/FloatingText/Main.php
@@ -20,6 +20,33 @@ use pocketmine\math\Vector3;
 
 class Main extends PluginBase implements Listener{
 
+/*	
+   public function translateColors($symbol, $color){
+	$color = str_replace($symbol."0", TextFormat::BLACK, $color);
+	$color = str_replace($symbol."1", TextFormat::DARK_BLUE, $color);
+	$color = str_replace($symbol."2", TextFormat::DARK_GREEN, $color);
+	$color = str_replace($symbol."3", TextFormat::DARK_AQUA, $color);
+	$color = str_replace($symbol."4", TextFormat::DARK_RED, $color);
+	$color = str_replace($symbol."5", TextFormat::DARK_PURPLE, $color);
+	$color = str_replace($symbol."6", TextFormat::GOLD, $color);
+	$color = str_replace($symbol."7", TextFormat::GRAY, $color);
+	$color = str_replace($symbol."8", TextFormat::DARK_GRAY, $color);
+	$color = str_replace($symbol."9", TextFormat::BLUE, $color);
+	$color = str_replace($symbol."a", TextFormat::GREEN, $color);
+	$color = str_replace($symbol."b", TextFormat::AQUA, $color);
+	$color = str_replace($symbol."c", TextFormat::RED, $color);
+	$color = str_replace($symbol."d", TextFormat::LIGHT_PURPLE, $color);
+	$color = str_replace($symbol."e", TextFormat::YELLOW, $color);
+	$color = str_replace($symbol."f", TextFormat::WHITE, $color);
+	$color = str_replace($symbol."k", TextFormat::OBFUSCATED, $color);
+	$color = str_replace($symbol."l", TextFormat::BOLD, $color);
+	$color = str_replace($symbol."m", TextFormat::STRIKETHROUGH, $color);
+	$color = str_replace($symbol."n", TextFormat::UNDERLINE, $color);
+	$color = str_replace($symbol."o", TextFormat::ITALIC, $color);
+	$color = str_replace($symbol."r", TextFormat::RESET, $color);
+	return $color;
+   }
+*/   
    public function onLoad(){
         $this->getLogger()->info(TextFormat::GREEN. "FloatingText has been enabled.");  
    }
